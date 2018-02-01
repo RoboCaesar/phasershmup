b105fe36c9958874ec09accfa2e75a27ecc6759e
RoboCaesar <twdrax@gmail.com>
2018-02-01 13:37:58 +0700

    initial

---
+++ myscript.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create, update: update });
+
+function preload() {
+    //game.load.image('hero', 'assets/sprites/hero.png');
+    //game.load.image('sky', 'assets/sprites/sky2.png');
+}
+
+function create() {
+}
+
+function update() {
+
+}
